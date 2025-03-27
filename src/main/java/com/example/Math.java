08950a992989f2cdcbf3d035f5e0ec8d6448299e
Angel-de-la-rosa <angeldelarosa.trabajo@gmail.com>
2025-03-27 23:16:58 +0100

    jenkinsfile fix2

--- src/main/java/com/example/Math.java
+++ src/main/java/com/example/Math.java
@@ -5,7 +5,7 @@ public class Math {
         System.out.println("hola jenkins que tal");
 
         int resultantMultiplication = multiplicacion(5, 3);
-        System.out.println("la suma de 5 + 3 es: " + resultantMultiplication);
+        System.out.println("la multiplicacion de 5 + 3 es: " + resultantMultiplication);
     }
 
     public static int multiplicacion(int a, int b){
